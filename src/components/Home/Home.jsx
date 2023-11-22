eb9a779bc7458c821c07091bfdb02222b860aa56
Amir Pasha Zarghami <apashazarghami@gmail.com>
2023-11-22 20:22:17 +0330

    init sidebar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 import styles from './Home.module.css';
 import Header from "../header/Header";
 import Main from "../main/Main";
+import Sidebar from '../sidebar/Sidebar';
 
 const Home = () => {
     return(
-        <div className={styles.container}>
+        <div>
             <Header />
-            <Main />
+            <div className={styles.layout}>
+                <Sidebar />
+                <Main />
+            </div>
         </div>
     )
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+import styles from './Sidebar.module.css';
+
+const Sidebar = () => {
+    return(
+        <aside className={styles.container}></aside>
+    )
+}
+
+export default Sidebar;
